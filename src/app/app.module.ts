b9b642051df691fe89ad467cef3e6aec571cfb66
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-09-26 16:23:45 +0330

    commit_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { EntryEditorComponent } from './entry-editor/entry-editor.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkoutsComponent,
+    EntryEditorComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
