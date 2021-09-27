f4835c9caf205ef527353ead2699ef4a1a4e4ac6
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-09-27 13:16:43 +0330

    commit_3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
+// Third party imports
+import {NgxLoadingModule} from 'ngx-loading';
+import {NgModel} from "@angular/forms";
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDbService} from "angular-in-memory-web-api";
+
+// App imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 import { EntryEditorComponent } from './entry-editor/entry-editor.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import {WorkoutsApiService} from "./services/workouts-api.service";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+
 
 @NgModule({
   declarations: [
@@ -18,9 +30,13 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgxLoadingModule.forRoot({}),
+    NgbModule
   ],
-  providers: [],
+  providers: [WorkoutsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
